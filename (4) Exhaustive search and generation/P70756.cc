a2d93238bbdb07dda16f5a530095b38e2db776f7
maziu-7 <0gtkr0t@protonmail.com>
2024-05-07 15:36:38 +0200

    .

--- (4) Exhaustive search and generation/P70756.cc
+++ (4) Exhaustive search and generation/P70756.cc
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-void write(const vector<string>& v, const vector<int>& sol, int n, int p) {
+typedef vector<string> VS;
+typedef vector<int> VI;
+typedef vector<bool> VB;
+
+void write(const VS& v, const VI& sol, int n, int p) {
     for (int i = 0; i < p; ++i) {
         cout << "subset " << i+1 << ": {";
         bool found = true;
@@ -19,23 +22,22 @@ void write(const vector<string>& v, const vector<int>& sol, int n, int p) {
     cout << endl;
 }
 
-void particions(vector<string>& v, vector<int>& sol, int n, int p, int i) {
+void particions(VS& v, VI& sol, int n, int p, int i) {
     if (n == i) write(v, sol, n, p);
     else {
         for (int j = 0; j < p; ++j) {
             sol[i] = j;
-            particions(v, sol, n, p, i+1); 
+            particions(v, sol, n, p, i+1);
         }
     }
 }
 
 int main() {
-    int n;
+    int n,p;
     cin >> n;
-    vector<string> v(n);
+    VS v(n);
     for (int i = 0; i < n; ++i) cin >> v[i];
-    int p;
     cin >> p;
-    vector<int> sol(n);
+    VI sol(n);
     particions(v, sol, n, p, 0);
 }
