3b4888d28576dc12e6e9594c0e8e97e5e5cd3193
maziu-7 <0gtkr0t@protonmail.com>
2024-05-07 15:23:07 +0200

    .

--- (4) Exhaustive search and generation/P69348.cc
+++ (4) Exhaustive search and generation/P69348.cc
@@ -2,20 +2,23 @@
 #include <vector>
 using namespace std;
 
-void escriu_perm(const vector<int>& v) {	
+typedef vector<int> VI;
+typedef vector<bool> VB;
+
+void write(const vector<int>& v) {	
 	cout << '(';
 	for (int i = 0; i < v.size()-1; ++i) cout << v[i] << ',';
 	cout << v[v.size()-1] << ')' << endl;
 }
 
-void permutations(vector<int>& v, int i, int n, vector<bool>& used) {
-	if (i == v.size()) escriu_perm(v);
+void permutations(int i, VI& sol, VB& used, int n) {
+	if (i == n) write(v);
 	else {
 		for (int j = 1; j <= n; ++j) {
 			if (not used[j-1]) {
-				v[i] = j;
 				used[j-1] = true;
-				permutations(v, i+1, n, used);
+				sol[i] = j;
+				permutations(i+1, sol, used, n);
 				used[j-1] = false;
 			}
 		}
@@ -25,7 +28,7 @@ void permutations(vector<int>& v, int i, int n, vector<bool>& used) {
 int main() {
 	int n;
 	cin >> n;
-	vector<int> v(n);
-	vector<bool> used(n, false);
-	permutations(v, 0, n, used);
+	VI sol(n);
+	VB used(n, false);
+	permutations(0, sol, used, n);
 }
