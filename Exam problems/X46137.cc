0716040e0d32b9b1f152f911ecf3c3ab5c95c584
maziu-7 <0gtrkt0@protonmail.com>
2024-05-30 02:08:46 +0200

    .

--- Exam problems/X46137.cc
+++ Exam problems/X46137.cc
@@ -3,12 +3,12 @@
 using namespace std;
 
 void par_comp(int i, string& sol, int n, int na, int nb) {
-    if (abs(na-n) > 2) return;
-    else (i == n) cout << sol << endl;
+    if (abs(na-nb) > 2) return;
+    else if (i == n) cout << sol << endl;
     else {
         sol[i] = 'a';
         par_comp(i+1, sol, n, na+1, nb);
-        
+
         sol[i] = 'b';
         par_comp(i+1, sol, n, na, nb+1);
     }
